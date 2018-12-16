8bdb5dd4b93c393b5e5e313d7588a63d1d961cc9
zhangzejun3 <zzj2472@163.com>
2018-12-16 23:13:30 +0800

    socket.io实现简单聊天功能

---
+++ demo6/wsServer.js
@@ -0,0 +1,56 @@
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+
+var PORT = 8001
+var clientCount = 0
+
+app.listen(PORT);
+
+io.on('connection', function (socket) {
+	clientCount++
+	socket.nickname = 'user' + clientCount
+	io.emit('enter', socket.nickname+' comes in');
+	socket.on('message', function (str) {
+		io.emit('message',`${socket.nickname} says: ${str}`)
+	});
+	socket.on('disconnect', function () {
+		io.emit('leave',socket.nickname + ' left')
+	});
+});
+
+
+// var server = ws.createServer(function (conn) {
+// 	console.log("New connection")
+// 	clientCount++
+// 	conn.nickname = 'user' + clientCount
+// 	var message = {}
+// 	message.type = 'enter'
+// 	message.data = conn.nickname+' comes in'
+// 	broadcast(JSON.stringify(message))
+// 	conn.on("text", function (str) {
+// 		console.log("Received " + str)
+// 		var message = {}
+// 		message.type = 'message'
+// 		message.data = `${conn.nickname} says: ${str}`
+// 		broadcast(JSON.stringify(message))
+// 	})
+// 	conn.on("close", function (code, reason) {
+// 		console.log("Connection closed")
+// 		var message = {}
+// 		message.type = 'leave'
+// 		message.data = conn.nickname + ' left'
+// 		broadcast(JSON.stringify(message))
+//     })
+//     conn.on("error", function (error) {
+//         console.log("handle error")
+//         console.log(error)
+// 	})
+// }).listen(PORT)
+
+console.log("websocket server listening on port" + PORT)
+
+// function broadcast(str){
+// 	server.connections.forEach((connection)=>{
+// 		connection.sendText(str)
+// 	})
+// }
