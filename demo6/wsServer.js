b2d9b2a2830ed41ebe4f25a85dee390f6a4075f3
zhangzejun3 <zzj2472@163.com>
2018-12-18 19:30:32 +0800

    delete

--- demo6/wsServer.js
+++ demo6/wsServer.js
@@ -18,39 +18,5 @@ io.on('connection', function (socket) {
 	});
 });
 
-
-// var server = ws.createServer(function (conn) {
-// 	console.log("New connection")
-// 	clientCount++
-// 	conn.nickname = 'user' + clientCount
-// 	var message = {}
-// 	message.type = 'enter'
-// 	message.data = conn.nickname+' comes in'
-// 	broadcast(JSON.stringify(message))
-// 	conn.on("text", function (str) {
-// 		console.log("Received " + str)
-// 		var message = {}
-// 		message.type = 'message'
-// 		message.data = `${conn.nickname} says: ${str}`
-// 		broadcast(JSON.stringify(message))
-// 	})
-// 	conn.on("close", function (code, reason) {
-// 		console.log("Connection closed")
-// 		var message = {}
-// 		message.type = 'leave'
-// 		message.data = conn.nickname + ' left'
-// 		broadcast(JSON.stringify(message))
-//     })
-//     conn.on("error", function (error) {
-//         console.log("handle error")
-//         console.log(error)
-// 	})
-// }).listen(PORT)
-
 console.log("websocket server listening on port" + PORT)
 
-// function broadcast(str){
-// 	server.connections.forEach((connection)=>{
-// 		connection.sendText(str)
-// 	})
-// }
